462d2cc6ebe1438871462a21e564b12831fd539e
yoshikaneichimura <yoshikanemobile@gmail.com>
2025-02-05 04:44:47 +0000

    [Add] CRU

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root 'homes#top'
+  get 'lists/new'
+  post 'lists' => 'lists#create'
+  get 'lists/index' => 'lists#index'
+  get 'lists/:id' => 'lists#show', as: 'list'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  delete 'lists/:id' => 'list#destroy', as: 'destroy_list'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
